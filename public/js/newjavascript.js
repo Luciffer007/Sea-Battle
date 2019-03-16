c99646e757e4626544b6bb8e64a2ad59b63ccf11
Artyom Gerasimov <42830086+Luciffer007@users.noreply.github.com>
2019-03-16 17:19:19 +0300

    Placement of ships on the field

--- public/js/newjavascript.js
+++ public/js/newjavascript.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function () {
-    function createField(fieldID) {
+    function createField(fieldID, side) {
         var field = document.getElementById(fieldID);
 
         /* Create top header */
@@ -14,7 +14,10 @@
         /* Create the rest part of the field */
         for (let i = 0; i < 110; i++) {
             if (i % 11) {
-                field.insertAdjacentHTML('beforeEnd', '<div class="not-checked" id="' + fieldID + '-' + Math.trunc(i / 11) + (i % 11 - 1) + '"></div>');
+                if (side == 0)
+                    field.insertAdjacentHTML('beforeEnd', '<div class="sea" id="' + fieldID + '-' + Math.trunc(i / 11) + (i % 11 - 1) + '"></div>');
+                else
+                    field.insertAdjacentHTML('beforeEnd', '<div class="not-checked" id="' + fieldID + '-' + Math.trunc(i / 11) + (i % 11 - 1) + '"></div>');
             } else {
                 field.insertAdjacentHTML('beforeEnd', '<div class="header">' + Math.trunc(i / 11 + 1) + '</div>');
             }
@@ -29,7 +32,6 @@
             if (event.target.hasAttribute('ship')) {
                 /* Hit */
                 event.target.removeAttribute('class');
-                event.target.setAttribute('class', 'ship');
                 event.target.insertAdjacentHTML('beforeEnd', '<div class="hit"></div>');
             } else {
                 /* Miss */
@@ -43,47 +45,74 @@
     /* Placement of ships on the field */
     function putShips(fieldID) {
         let ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
-        let tempFlag = false;
+        let completeFlag;
         let rowIndex;
         let columnIndex;
         let direction;
-
-        while (tempFlag == false) {
-            rowIndex = getRandomInt(0, 9);
-            columnIndex = getRandomInt(0, 9);
-            direction = getRandomInt(0, 1);
-            tempFlag = putShip(4, fieldID, rowIndex, columnIndex, direction);
-        }
+        ships.forEach(function(ship) {
+            completeFlag = false;
+            while (completeFlag == false) {
+                rowIndex = getRandomInt(0, 9);
+                columnIndex = getRandomInt(0, 9);
+                direction = getRandomInt(0, 1);
+                completeFlag = putShip(ship, fieldID, rowIndex, columnIndex, direction);
+            }
+        })
     }
 
     function putShip(typeShip, fieldID, rowIndex, columnIndex, direction) {
         let cellField = getCellField(fieldID, rowIndex, columnIndex);
-        if (cellField.hasAttribute('ship')) return false;
-        cellField.setAttribute('ship', '');
+        let cellShipBuffer = [];
+        /* One-cell zone around the ship */
+        let cellBuffer = [];
+
+        if (cellField.hasAttribute('ship') || cellField.hasAttribute('notTouch')) return false;
+        cellShipBuffer.push(cellField);
+        getOneCellZone(cellBuffer, fieldID, rowIndex, columnIndex);
 
         if (typeShip > 1) {
             if (direction == 0) {
                 if (columnIndex < (typeShip - 1)) {
                     for (let i = 0; i < (typeShip - 1); i++) {
-                        getCellField(fieldID, rowIndex, ++columnIndex).setAttribute('ship', '');
+                        cellField = getCellField(fieldID, rowIndex, ++columnIndex);
+                        if (cellField.hasAttribute('ship') || cellField.hasAttribute('notTouch')) return false;
+                        cellShipBuffer.push(cellField);
+                        getOneCellZone(cellBuffer, fieldID, rowIndex, columnIndex);
                     }
                 } else {
                     for (let i = 0; i < (typeShip - 1); i++) {
-                        getCellField(fieldID, rowIndex, --columnIndex).setAttribute('ship', '');
+                        cellField = getCellField(fieldID, rowIndex, --columnIndex);
+                        if (cellField.hasAttribute('ship') || cellField.hasAttribute('notTouch')) return false;
+                        cellShipBuffer.push(cellField);
+                        getOneCellZone(cellBuffer, fieldID, rowIndex, columnIndex);
                     }
                 }
             } else {
                 if (rowIndex < (typeShip - 1)) {
                     for (let i = 0; i < (typeShip - 1); i++) {
-                        getCellField(fieldID, ++rowIndex, columnIndex).setAttribute('ship', '');
+                        cellField = getCellField(fieldID, ++rowIndex, columnIndex);
+                        if (cellField.hasAttribute('ship') || cellField.hasAttribute('notTouch')) return false;
+                        cellShipBuffer.push(cellField);
+                        getOneCellZone(cellBuffer, fieldID, rowIndex, columnIndex);
                     }
                 } else {
                     for (let i = 0; i < (typeShip - 1); i++) {
-                        getCellField(fieldID, --rowIndex, columnIndex).setAttribute('ship', '');
+                        cellField = getCellField(fieldID, --rowIndex, columnIndex);
+                        if (cellField.hasAttribute('ship') || cellField.hasAttribute('notTouch')) return false;
+                        cellShipBuffer.push(cellField);
+                        getOneCellZone(cellBuffer, fieldID, rowIndex, columnIndex);
                     }
                 }
             }
         }
+
+        cellShipBuffer.forEach(function(cellShip) {
+            cellShip.setAttribute('ship', '');
+        });
+        cellBuffer.forEach(function(cell) {
+            if (cell == null || cell.hasAttribute('ship')) return;
+            cell.setAttribute('notTouch', '');
+        });
         return true;
     }
 
@@ -92,15 +121,27 @@
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    function getOneCellZone(bufferArray, fieldID, rowIndex, columnIndex) {
+        bufferArray.push(getCellField(fieldID, rowIndex-1, columnIndex));
+        bufferArray.push(getCellField(fieldID, rowIndex+1, columnIndex));
+        bufferArray.push(getCellField(fieldID, rowIndex, columnIndex+1));
+        bufferArray.push(getCellField(fieldID, rowIndex, columnIndex-1));
+        bufferArray.push(getCellField(fieldID, rowIndex-1, columnIndex+1));
+        bufferArray.push(getCellField(fieldID, rowIndex-1, columnIndex-1));
+        bufferArray.push(getCellField(fieldID, rowIndex+1, columnIndex-1));
+        bufferArray.push(getCellField(fieldID, rowIndex+1, columnIndex+1));
+        return bufferArray;
+    }
+
     function getCellField(fieldID, rowIndex, columnIndex) {
         let cellID = fieldID + '-' + String(rowIndex) + String(columnIndex);
         return document.getElementById(cellID);
     }
 
-    createField('my-fleet');
+    createField('my-fleet', 0);
     putShips('my-fleet');
 
-    createField('enemy-fleet');
+    createField('enemy-fleet', 1);
     putShips('enemy-fleet');
 }());
 
